645a71647f5cfb7d13e124f1ad51e40fe60a0b28
Dao Wei Lim <109159208+DaoWeiLim89@users.noreply.github.com>
2026-02-26 10:47:26 +0000

    Finished Assignment

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # DownloadImagesParallel
 In class assignment
+
+
+Dependencies:
+
+Package            Version
+------------------ ---------
+certifi            2026.2.25
+charset-normalizer 3.4.4
+idna               3.11
+pip                26.0.1
+requests           2.32.5
+urllib3            2.6.3
+tqdm               4.67.3
+
+To run the program, set up the venv first
+python3 -m venv venv
+source venv/bin/activate
+
+And then change images to set the range of images you want to download.
+Then run:
+python3 fastFetch.py
---
+++ fastFetch.py
@@ -0,0 +1,76 @@
+import requests
+from datetime import datetime
+import certifi
+import threading
+import concurrent.futures
+import time
+import tqdm
+import os
+
+# Variable id is used to select which image to download, and to name the file.
+# E.g if id = 300, it will download https://picsum.photos/300 and save it as image_300.jpg
+url = "https://picsum.photos/"
+filename = "image_"
+
+lock = threading.Lock()
+def log(status, id, timestamp):
+    with lock:
+        with open("logger.txt", "a") as f:
+            f.write(f"{timestamp} | {url}{id} | {filename}{id}.jpg | {status}\n")
+
+        #print(f"Downloaded: {imagesDownloaded}/{total_images}")
+
+def download_image(id: int):
+    try:
+        # timeout=3 means: wait max 3 seconds
+        response = requests.get(url + str(id), timeout=3, verify=certifi.where())
+
+        with open(f"images/{filename}{id}.jpg", "wb") as f:
+            f.write(response.content)
+
+        log("SUCCESS", id, datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+    except requests.exceptions.Timeout:
+        log("TIMEOUT", id, datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+    except Exception:
+        log("FAILED", id, datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+def serialDownload(images):
+    start_time = time.perf_counter()
+    for id in tqdm.tqdm(range(images[0], images[1]), desc="Downloading Serial"):
+        download_image(id)
+
+    end_time = time.perf_counter()
+    print(f"Serial download took {end_time - start_time:.2f} seconds")
+
+def parallelDownload(images):
+    total_images = images[1] - images[0]
+    start_time = time.perf_counter()
+    imageIDs = [id for id in range(images[0], images[1])]
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        results = list(tqdm.tqdm(
+            executor.map(download_image, imageIDs),
+            total=total_images,
+            desc="Downloading Parallel"
+            ))
+
+    end_time = time.perf_counter()
+    print(f"Parallel download took {end_time - start_time:.2f} seconds")
+
+if __name__ == "__main__":
+    if not os.path.exists("images"):
+        os.makedirs("images")
+
+    '''
+    if os.path.exists("images"):
+        os.rmdir("images") # find a way to delete a full folder
+        os.makedirs("images")
+    '''
+    
+    if os.path.exists("logger.txt"):
+        os.remove("logger.txt")
+
+    images = [100, 150]
+    serialDownload(images)
+    parallelDownload(images)
